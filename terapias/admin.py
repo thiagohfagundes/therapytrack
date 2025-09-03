d8fefb4015a180161bf72155bb85a3bebed1620a
thiagohfagundes <eng.thiagofagundes@gmail.com>
2025-09-03 18:50:16 -0300

    Primeiro commit com gitignore

--- terapias/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Evento, Profissional, Clinica, Rotina, RotinaItem
-
-# Register your models here.
-admin.site.register(Evento)
-admin.site.register(Profissional)
-admin.site.register(Clinica)
-admin.site.register(Rotina)
-admin.site.register(RotinaItem)
